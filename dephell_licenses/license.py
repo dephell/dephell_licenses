67c01e00eaa41b3610d0aa6ad79dd6605459e47b
Gram <master_fess@mail.ru>
2019-03-25 20:35:21 +0300

    improve generator

--- dephell_licenses/license.py
+++ dephell_licenses/license.py
@@ -1,6 +1,28 @@
 import attr
+import requests
+from cached_property import cached_property
 
 
 @attr.s()
 class License:
-    ...
+    id = attr.ib(type=str)
+    classifier = attr.ib(type=str)
+    name = attr.ib(type=str)
+
+    approved = attr.ib(type=bool)
+    deprecated = attr.ib(type=bool)
+
+    links = attr.ib(type=tuple)
+    url = attr.ib(type=str)
+
+    @cached_property
+    def _details(self):
+        requests.get(self.url).json()
+
+    @property
+    def template(self):
+        return self._details['standardLicenseTemplate']
+
+    @property
+    def comments(self):
+        return self._details['licenseComments']
--- generate.py
+++ generate.py
@@ -1,7 +1,8 @@
 import json
 import requests
+import re
 from pathlib import Path
-from textdistance import levenshtein
+from textdistance import hamming
 from tqdm import tqdm
 
 url = 'https://raw.githubusercontent.com/spdx/license-list-data/master/json/licenses.json'
@@ -10,17 +11,63 @@ info = requests.get(url).json()['licenses']
 url = 'https://pypi.org/pypi?%3Aaction=list_classifiers'
 classifiers = requests.get(url).text.split('\n')
 
+
+def get_by_name(name):
+    best_license = info[0]
+    best_distance = float('Inf')
+    for license in info:
+        d = hamming.normalized_distance(name, license['name'])
+        if d < best_distance:
+            best_license = license
+            best_distance = d
+    if best_distance < .90:
+        return best_license
+
+
+def get_by_fullname(name):
+    best_license = info[0]
+    best_distance = float('Inf')
+    for license in info:
+        d = hamming.normalized_distance(name, license['name'] + ' ' + license['licenseId'])
+        if d < best_distance:
+            best_license = license
+            best_distance = d
+    if best_distance < .90:
+        return best_license
+
+
+rex_clean = re.compile(r'[^a-zA-Z0-9]+')
+
+
+def get_by_id(name):
+    if '(' not in name:
+        return
+    name = name.split('(')[-1].split(')')[0]
+    name = rex_clean.sub('', name)
+    best_license = info[0]
+    best_distance = float('Inf')
+    for license in info:
+        d = hamming.distance(name, rex_clean.sub('', license['licenseId']))
+        if d < best_distance:
+            best_license = license
+            best_distance = d
+    if best_distance <= 1:
+        return best_license
+
+
 licenses = []
 for classifier in tqdm([c for c in classifiers if c.strip().startswith('License :: ')]):
     classifier = classifier.strip()
     name = classifier.split('::')[-1].strip()
-    best_license = info[0]
-    best_distance = float('Inf')
-    for license in info:
-        d = levenshtein(name, license['name'] + ' ' + license['licenseId'])
-        if d < best_distance:
-            best_license = license
-            best_distance = d
+
+    best_license = get_by_id(name)
+    if best_license is None:
+        best_license = get_by_name(name)
+    if best_license is None:
+        best_license = get_by_fullname(name)
+    if best_license is None:
+        continue
+
     licenses.append(dict(
         approved=best_license['isOsiApproved'],
         classifier=classifier,
