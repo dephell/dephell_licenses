27a54edd2d57c48268abb4e5cc6b82f4fd54c30e
Gram <master_fess@mail.ru>
2019-03-26 09:59:50 +0300

    +text maker

--- dephell_licenses/_license.py
+++ dephell_licenses/_license.py
@@ -1,6 +1,18 @@
+import re
 import attr
 import requests
 from cached_property import cached_property
+
+
+# <<var;name="copyright";original="<year> <copyright holders>";match=".+">>
+# <<var;name=organizationClause3;original=the copyright holder;match=.+>>
+rex_var = re.compile(
+    r'<<var;'
+    r'name="?(?P<name>[a-zA-Z0-9]+)"?;'
+    r'original="?(?P<original>.+?)"?;'
+    r'match="?(?P<match>.+?)"?>>'
+)
+rex_junk = re.compile(r'<<.+?>>')
 
 
 @attr.s()
@@ -17,7 +29,7 @@ class License:
 
     @cached_property
     def _details(self):
-        requests.get(self.url).json()
+        return requests.get(self.url).json()
 
     @property
     def template(self):
@@ -26,3 +38,20 @@ class License:
     @property
     def comments(self):
         return self._details['licenseComments']
+
+    @cached_property
+    def vars(self):
+        vars = []
+        for match in rex_var.finditer(self.template):
+            var = match.groupdict()
+            var['text'] = match.group(0)
+            vars.append(var)
+        return vars
+
+    def make_text(self, **kwargs) -> str:
+        text = self.template
+        for var in self.vars:
+            value = kwargs.get(var['name'], var['original'])
+            text = text.replace(var['text'], value)
+        text = rex_junk.sub('', text)
+        return text
---
+++ tests/test_license.py
@@ -0,0 +1,11 @@
+from dephell_licenses import licenses
+
+
+def test_make_text():
+    license = licenses.get_by_id('MIT')
+
+    assert 'Copyright (c) <year> <copyright holders>' in license.make_text()
+    assert 'beginOptional' not in license.make_text()
+    assert license.make_text().strip().startswith('MIT License')
+
+    assert 'Copyright (c) 2018 Gram' in license.make_text(copyright="2018 Gram")
